2707f9de8768e13dcaa95428885aef69e6370f78
Roman Geathers <romangeathersdev@gmail.com>
2022-11-07 10:26:23 -0500

    Reset all chosen states when score is Reset
    Reset score to 0 if user selects same card twice in game. If score is
    reset to 0, then reset the chosen states to false for every card.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import Card from './components/Card'
 
 const App = () => {
   const [score, setScore] = useState(0)
+  const resetChosen = false
+
+  const parentCallbacks ={
+    updateScore: () => {
+      setScore(score + 1)
+    },
+    resetScore: () => {
+      setScore(0)
+    },
+  }
 
   const cardArray = []
   for (let i = 0; i < 12; i++) {
-    cardArray.push(<Card id={i} image={i} />)
+    cardArray.push(<Card id={i} image={i} parentCallbacks={parentCallbacks} score={score} />)
   }
 
-  const resetChosen = () => {
-
-  }
+  // useEffect(score => {
+  //   if (score === 0) {
+      
+  //   }
+  // }, [score])
 
   return (
     <div className="App">
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,27 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
-const Card = ({ image, id }) => {
+const Card = ({ image, id, parentCallbacks, score }) => {
   const [chosen, setChosen] = useState(false)
 
+  const handleClick = () => {
+    if (chosen === false) {
+      setChosen(true)
+      parentCallbacks.updateScore()
+      console.log(score)
+    } else {
+      console.log('reset')
+      parentCallbacks.resetScore()
+    }
+  }
+
+  useEffect(() => {
+    if (score === 0) {
+      setChosen(false)
+    }
+  }, [score])
+
   return (
-    <div onClick={() => setChosen(true)}>{image}</div>
+    <div onClick={handleClick}>{image}</div>
   )
 }
 
