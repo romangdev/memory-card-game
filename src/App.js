c33052cd53394143d086969b72966dd8b92e5a12
Roman Geathers <romangeathersdev@gmail.com>
2022-11-07 17:43:39 -0500

    Display cards to screen randomly
    Note that there's a bug with interactivity. Cards not redisplaying
    in random order on click, nor is score updating properly.

--- src/App.js
+++ src/App.js
@@ -5,43 +5,45 @@ import Card from './components/Card'
 const App = () => {
   const [score, setScore] = useState(0)
   const [bestScore, setBestScore] = useState(0)
-  // const [displayedCardOrder, setDisplayedCardOrder] = useState([])
 
   const parentCallbacks ={
     updateScore: () => setScore(score + 1),
     resetScore: () => setScore(0),
   }
 
-  const cardArray = []
+  const cards = []
   for (let i = 0; i < 12; i++) {
-    cardArray.push(<Card id={i} image={i} parentCallbacks={parentCallbacks} score={score} />)
+    cards.push(<Card id={i} image={i} parentCallbacks={parentCallbacks} score={score} />)
   }
 
-  // const setRandomCardOrder = (cardArray) => {
-  //   let usedIndexes = []
-  //   let randomIndex
-  //   let displayedCardOrder = []
+  const [cardArray] = useState(cards)
+  const [displayedCardOrder, setDisplayedCardOrder] = useState([])
 
-  //   for (let i = 0; i < cardArray.length; i++) {
-  //     do {
-  //       randomIndex = Math.floor(Math.random() * 12)
-  //       if (!usedIndexes.includes(randomIndex)) {
-  //         displayedCardOrder.push(cardArray[randomIndex])
-  //       }
-  //     } while (usedIndexes.includes(randomIndex))
+  const setRandomCardOrder = (cardArray) => {
+    let usedIndexes = []
+    let randomIndex
+    let displayedCardOrder = []
+
+    for (let i = 0; i < cardArray.length; i++) {
+      do {
+        randomIndex = Math.floor(Math.random() * 12)
+        if (!usedIndexes.includes(randomIndex)) {
+          displayedCardOrder.push(cardArray[randomIndex])
+        }
+      } while (usedIndexes.includes(randomIndex))
       
-  //     usedIndexes.push(randomIndex)
-  //   }
-  //   return displayedCardOrder
-  // }
+      usedIndexes.push(randomIndex)
+    }
+    return displayedCardOrder
+  }
 
   useEffect(() => {
     if (score > bestScore) setBestScore(score)
   }, [score, bestScore])
 
-  // useEffect(() => {
-  //   setDisplayedCardOrder(setRandomCardOrder(cardArray))
-  // }, [cardArray])
+  useEffect(() => {
+    setDisplayedCardOrder(setRandomCardOrder(cardArray))
+  }, [cardArray, score])
 
   return (
     <div className="App">
@@ -55,14 +57,14 @@ const App = () => {
       <main>
         <p>Click each image only once to earn points. Don't click any images more than once!</p>
         <div className='card-board'>
-          {/* {displayedCardOrder.map(card => {
+          {displayedCardOrder.map(card => {
             return <div key={card.props.id}>{card}</div>
-          })} */}
+          })}
 
-          {cardArray.map((card, i) => {
+          {/* {cardArray.map((card, i) => {
             console.log(card)
             return <div key={i}>{card}</div>
-          })}
+          })} */}
         </div>
       </main>
     </div>
